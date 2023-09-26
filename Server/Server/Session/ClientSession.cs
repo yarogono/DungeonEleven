e165a5d1bc2ba8abcf3f2caf6c975db88bb88473
yarogono <70641418+yarogono@users.noreply.github.com>
2023-09-26 18:53:54 +0900

    feature/Server초기 코드작성

---
+++ Server/Server/Session/ClientSession.cs
@@ -0,0 +1,31 @@
+﻿using ServerCore;
+using System.Net;
+
+namespace Server.Session
+{
+    public class ClientSession : PacketSession
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+
+            Thread.Sleep(5000);
+            Disconnect();
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnRecvPacket(ArraySegment<byte> buffer)
+        {
+            PacketManager.Instance.OnRecvPacket(this, buffer);
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+}
