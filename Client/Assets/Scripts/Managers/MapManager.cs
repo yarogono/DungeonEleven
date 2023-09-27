95074719044c369c835715cdf622f5912210e1c7
이경민(Kyung Min Lee) <kminsmin@gmail.com>
2023-09-27 17:05:33 +0900

    map transition effect

--- Client/Assets/Scripts/Managers/MapManager.cs
+++ Client/Assets/Scripts/Managers/MapManager.cs
@@ -8,6 +8,8 @@ public class MapManager : MonoBehaviour
     [SerializeField] private GameObject _player;
     [SerializeField] private GameObject[] _maps;
     [SerializeField] private GameObject _startMap;
+    [SerializeField] private GameObject _mapTransitionImage;
+    private MapTransition _mapTransition;
 
     private void Awake()
     {
@@ -17,12 +19,16 @@ public class MapManager : MonoBehaviour
     {
         _startMap.SetActive(true);
         _player.transform.localPosition = Vector3.zero;
+        _mapTransition = _mapTransitionImage.GetComponent<MapTransition>();
+
     }
 
     public void LoadNextMap()
     {
         _startMap.SetActive(false);
+        _mapTransition.LoadingMap();
         _maps[0].SetActive(true);
+        _mapTransition.LoadedMap();
         _player.transform.localPosition = Vector3.zero;
 
     }
